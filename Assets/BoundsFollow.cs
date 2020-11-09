845d46804782c32d29bc73475d061d73075e8cd5
DESKTOP-Q6B83EK\AAA <inuyashazaq@gmail.com>
2020-11-09 17:06:38 +0800

    add bounds

---
+++ Assets/BoundsFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoundsFollow : MonoBehaviour
+{
+    public GameObject bounds;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bounds.transform.position = transform.position;
+    }
+}
